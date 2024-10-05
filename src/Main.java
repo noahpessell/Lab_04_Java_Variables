533d53d9afc270be896c8bf269f892cc9621bd2f
noahpessell <nwpessell@mail.com>
2024-10-04 23:27:42 -0400

    added variables

--- src/Main.java
+++ src/Main.java
@@ -17,9 +17,36 @@ public class Main
         double doubleProduct = 42.33;
         double doubleDifference = 25.67;
         double doubleQuotient = 34.81;
+        double doubleModulo = 45.23;
+        int familyKids = 0;
+        boolean raining = false;
+        double gallonGasPrice = 0.00;
+        int favoriteNumber = 6;
+        double shoeSize = 0;
+        String birthMonth = "";
+        String fullName = "";
+
+        //ints
         intSum = intOperandA + intOperandB;
-        System.out.println("The sum using ints of " + intOperandA + " " + intOperandB + " is " + intSum);
-
-
+        System.out.println("The sum using ints of " + intOperandA + " and " + intOperandB + " is " + intSum);
+        intDifference = intOperandA - intOperandB;
+        System.out.println("The difference using ints of " + intOperandA + " and " + intOperandB + " is " + intDifference);
+        intProduct = intOperandA * intOperandB;
+        System.out.println("The product using ints of " + intOperandA + " and " + intOperandB + " is " + intProduct);
+        intQuotient = intOperandA / intOperandB;
+        System.out.println("The quotient using ints of " + intOperandA + " and " + intOperandB + " is " + intQuotient);
+        intModulo = intOperandA % intOperandB;
+        System.out.println("The modulo using ints of " + intOperandA + " and " + intOperandB + " is " + intModulo);
+        //doubles
+        doubleSum = doubleOperandA + doubleOperandB;
+        System.out.println("The sum using doubles of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleSum);
+        doubleDifference = doubleOperandA - doubleOperandB;
+        System.out.println("The difference using doubles of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleDifference);
+        doubleProduct = doubleOperandA * doubleOperandB;
+        System.out.println("The product using doubles of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleProduct);
+        doubleQuotient = doubleOperandA / doubleOperandB;
+        System.out.println("The quotient using doubles of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleQuotient);
+        doubleModulo = doubleOperandA % doubleOperandB;
+        System.out.println("The modulo using doubles of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleModulo);
     }
 }
