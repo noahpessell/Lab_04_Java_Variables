6cd99d0c1dc6ae27d6864a9fa62148903f1a810d
noahpessell <nwpessell@mail.com>
2024-10-04 23:01:35 -0400

    init commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int intOperandA = 1;
+        int intOperandB = 6;
+        int intSum = 21;
+        int intProduct = 42;
+        int intDifference = 25;
+        int intQuotient = 34;
+        int intModulo = 3;
+        double doubleOperandA = 1.22;
+        double doubleOperandB = 6.53;
+        double doubleSum = 21.09;
+        double doubleProduct = 42.33;
+        double doubleDifference = 25.67;
+        double doubleQuotient = 34.81;
+        intSum = intOperandA + intOperandB;
+        System.out.println("The sum using ints of " + intOperandA + " " + intOperandB + " is " + intSum);
+
+
+    }
+}
